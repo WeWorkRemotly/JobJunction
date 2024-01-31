66982694854385affd517f935e0001deeccc3f7f
aman0990 <raj.aman.726@gmail.com>
2024-01-31 14:00:59 +0530

    FirstCommit

--- src/main/java/in/jobjunction/controller/AdminDashboardController.java
+++ src/main/java/in/jobjunction/controller/AdminDashboardController.java
@@ -1,7 +1,9 @@
 package in.jobjunction.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminDashboardController {
 }
